19e66e64fcd36ca9f23f889badd32b86255bb346
Denis <dklymenko@s-pro.io>
2020-08-05 19:13:18 +0300

    first commit

---
+++ server/server/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('../schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3005;
+
+mongoose.connect('mongodb://Yauhen:Pass123@ds163835.mlab.com:63835/graphql-tutorial', { useNewUrlParser: true });
+
+app.use(cors());
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', err => console.log(`Connection error: ${err}`));
+dbConnection.once('open', () => console.log('Connected to DB!'));
+
+app.listen(PORT, err => {
+  err ? console.log(err) : console.log('Server started!');
+});
